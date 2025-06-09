41739bc8bbeeb632348952528d0040c97e40aa03
amitsingh76311 <124235547+amitsingh76311@users.noreply.github.com>
2025-06-09 08:04:25 +0000

    "learned datatypes and variables in js"

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234;
+let accountMail = "Amit@123";
+var accountPassword = "12345";
+accountCity = "Bihar";
+
+// accountId = 2; // whenever we can dealares const keyword  we cant changes it  or not allowed .
+ accountMail = "Bholu@123";
+ accountPassword= "321";
+ accountCity = "hyd";
+ let accountState;
+console.log(accountId);
+
+
+
+console.table([accountId,accountMail,accountPassword,accountCity,accountState]);
+
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+let name = "Amit";
+let age = 18;
+let isLoggedIn = false;
+
+
+// data types are :--
+  // this is below primitive datatypes 
+// number datetypes ;
+// string datatypes = "";
+// bigint ;
+// boolean = true/false;
+// null 
+// undefined:-
+// symboll : - unique datetypes :--
+
+
+// refrence data types are --
+
+// 1. object
+
+
+// console.log( typeof undefined);
+// console.log(typeof null);
+
+
+ 
